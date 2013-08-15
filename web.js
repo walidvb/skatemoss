da472f9d223014bb98bdf550ecff51bebddcf125
Walid <walidvb@gmail.com>
2013-08-16 01:58:41 +0200

    changed port number changed socket connection to server address

--- web.js
+++ web.js
@@ -3,16 +3,22 @@
 
 var express = require('express');
 var app = express();
-var port = 3700;
-
+var port = process.env.PORT || 3000;
 
 //include jade
 app.set('views', __dirname + "/tpl");
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-  res.render("page");
+
+app.get("/beamer", function(req, res){
+  res.render("beamer");
 })
+
+app.get("/controller", function(req, res){
+  res.render("controller");
+})
+
+
 
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
