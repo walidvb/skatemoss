9b87053c9d2ea96a22d35fd235f0ba5dc586af8c
Walid <walidvb@gmail.com>
2013-08-18 21:51:08 +0200

    commited the changes

--- public/controller.js
+++ public/controller.js
@@ -9,13 +9,14 @@
   var selectedClass = 'selected';
   items.on('click', function() {
     var id = $(this).attr('data-id');
+    var img = $(this).find('img').attr('src');
     $('.'+selectedClass).removeClass(selectedClass);
     $(this).addClass(selectedClass);
 
     socket.emit('send', {
-      id: id
+      id: id,
+      img: img,
     });
-    
   });
   setTimeout(function(){$('#main ul').masonry()}, 1000);
 });
